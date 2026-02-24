d34bdb7b7b41fa3eb5ada4ed90a5e5f48dfe860e
mtu <michal.tumilowicz01@gmail.com>
2026-02-24 21:24:05 +0100

    partial encryption pt18

--- src/test/groovy/org/example/TemporalWorkflowResourceIT.groovy
+++ src/test/groovy/org/example/TemporalWorkflowResourceIT.groovy
@@ -2,28 +2,30 @@ package org.example
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured
-import org.junit.jupiter.api.Test
-
-import static org.hamcrest.Matchers.equalTo
-import static org.hamcrest.Matchers.matchesRegex
-import static org.hamcrest.Matchers.notNullValue
+import spock.lang.Specification
 
 @QuarkusTest
-class TemporalWorkflowResourceIT {
+class TemporalWorkflowResourceIT extends Specification {
 
-    @Test
-    void returnsExampleWorkflowResponse() {
-        RestAssured.given()
+    def 'returns example workflow response'() {
+        when:
+        def body = RestAssured.given()
                 .contentType('application/json')
                 .body('{"name":"Temporal","apiKey":"sk_test_1234567890"}')
                 .when()
                 .post('/temporal/example')
                 .then()
                 .statusCode(200)
-                .body('oldName', equalTo('Temporal'))
-                .body('newName', equalTo('new_name_hardcoded'))
-                .body('oldApiKey', equalTo('sk_test_1234567890'))
-                .body('newApiKey', equalTo('sk_new_hardcoded_123'))
-                .body('date', notNullValue())
+                .extract()
+                .as(Map)
+
+        then:
+        with(body) {
+            oldName == 'Temporal'
+            newName == 'new_name_hardcoded'
+            oldApiKey == 'sk_test_1234567890'
+            newApiKey == 'sk_new_hardcoded_123'
+            date
+        }
     }
 }
